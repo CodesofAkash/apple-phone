78cbb0f132292df4a0fec28f8a36749821c8ddcf
Akash <akashcodesharma@gmail.com>
2026-02-23 20:39:08 +0530

    fix prisma config and datasource url

--- server/utils/prisma.js
+++ server/utils/prisma.js
@@ -2,4 +2,8 @@ import pkg from '@prisma/client'
 
 const { PrismaClient } = pkg
 
-export const prisma = new PrismaClient()
+const globalForPrisma = globalThis
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
