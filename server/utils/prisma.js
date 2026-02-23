5fe7c67f626d222bcb4a15b3aa9bd0fce628ee92
Akash <akashcodesharma@gmail.com>
2026-02-23 16:13:57 +0530

    update prisma.js

--- server/utils/prisma.js
+++ server/utils/prisma.js
@@ -1,9 +1,10 @@
-import { PrismaClient } from '../../src/generated/prisma/index.js'
+import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import pg from 'pg'
 
 const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  max: 10,
 })
 
 const adapter = new PrismaPg(pool)
