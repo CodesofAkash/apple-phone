443705ee6ef3814887120aa434c773a628389020
Akash <akashcodesharma@gmail.com>
2026-02-23 20:48:52 +0530

    fix: remove driverAdapters preview, no-arg PrismaClient constructor

--- server/utils/prisma.js
+++ server/utils/prisma.js
@@ -4,8 +4,6 @@ const { PrismaClient } = pkg
 
 const globalForPrisma = globalThis
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-})
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
