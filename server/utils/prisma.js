0fd0c4ba31b9fe239f752b44c216a500d797e9d8
Akash <akashcodesharma@gmail.com>
2026-02-23 21:21:44 +0530

    fix prisma config - remove adapter from datasource block

--- server/utils/prisma.js
+++ server/utils/prisma.js
@@ -1,9 +1,17 @@
 import pkg from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
 
 const { PrismaClient } = pkg
 
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const adapter = new PrismaPg(pool)
+
 const globalForPrisma = globalThis
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
