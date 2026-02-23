716bcdf8a6ebaef0bd2e4da6b75bef81bc4dddf3
Akash <akashcodesharma@gmail.com>
2026-02-23 20:19:10 +0530

    final commit

--- server/utils/prisma.js
+++ server/utils/prisma.js
@@ -1,14 +1,5 @@
 import pkg from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import pg from 'pg'
 
 const { PrismaClient } = pkg
 
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  max: 10,
-})
-
-const adapter = new PrismaPg(pool)
-
-export const prisma = new PrismaClient({ adapter })
+export const prisma = new PrismaClient()
