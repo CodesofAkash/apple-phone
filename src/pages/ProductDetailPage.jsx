bfe32664f68358f3f3e941a46f19a4c359825a9c
Akash <akashcodesharma@gmail.com>
2026-02-28 06:44:29 +0530

    fix: increase 3d model scale on product detail page

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -37,6 +37,7 @@ const ModelScene = ({ modelItem, selectedSize }) => {
       gl={{ antialias: true, powerPreference: 'high-performance' }}
       dpr={[1, 2]}
       frameloop="always"
+      camera={{ position: [0, 0, 4], fov: 35 }}
     >
       <ambientLight intensity={0.3} />
       <Lights />
@@ -46,11 +47,13 @@ const ModelScene = ({ modelItem, selectedSize }) => {
         rotateSpeed={0.4}
       />
       <Suspense fallback={<CanvasLoader />}>
-        <IPhone
-          scale={selectedSize === 'small' ? [15, 15, 15] : [17, 17, 17]}
-          item={modelItem}
-          size={selectedSize}
-        />
+        <group position={[0, 0, 0]}>
+          <IPhone
+            scale={selectedSize === 'small' ? [20, 20, 20] : [23, 23, 23]}
+            item={modelItem}
+            size={selectedSize}
+          />
+        </group>
       </Suspense>
     </Canvas>
   )
